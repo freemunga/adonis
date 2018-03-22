a11d28a4ad3bf8d31176e3a39879631a3e6a7e77
Adonis Richards <adonis.richards@outlook.com>
2018-03-22 08:49:07 +0100

    Added contact_process

---
+++ contact.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Adonis Richards</title>
+	<title>This is a second title</title>
+</head>
+<body>
+
+</body>
+</html>
---
+++ contact_process.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+	<title>My new Title</title>
+	<title>Another new Title</title>
+
+</head>
+
+<body>
+
+	<p>This is just texst</p>
+	<p>Some more tekst</p>
+
+
+</body>
+
+
+
+
+
+
+
+
+</html>
