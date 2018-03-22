64d4bc3e6554b0d1669586e2e541877c82dbff1a
Adonis Richards <adonis.richards@outlook.com>
2018-03-22 08:17:45 +0100

    vandaag 22-03-2018

---
+++ new_file.php
@@ -0,0 +1,21 @@
+<Doctype>
+<html>
+
+
+<head>
+	<title>My new document</title>
+	<title>My other new document</title>
+
+
+ </head>
+<body>
+
+<p>This is just text</p>
+<p>Some more text</p>
+</body>
+
+
+
+
+
+</html>
